fce2f9464056358474677a1b9972eceee9205189
Priya Nakhre <pn2bh@virginia.edu>
2017-11-01 13:34:06 -0400

    Fix script for time outs

--- batch-script.py
+++ batch-script.py
@@ -8,12 +8,21 @@ while True:
 	time.sleep(90) 
 	print("no ")
 	#es = Elasticsearch(['es'])
+
+	fixture = {'question' : "will it rain tomorrow?", 'category' : 'weather', 'description' : "weather tomorrow",	'per_person_cap': 78, 'id': 6}
 	
 	
 	es = Elasticsearch(['es'])
+	es.index(index='listing_index', doc_type='listing', id=fixture['id'], body=fixture)
 	
-	consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+	es.indices.refresh(index='listing_index')
+	try:
+		consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
 	
+	except:
+		time.sleep(20) 
+		consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+		
 
 	for message in consumer:
 	 	print(json.loads((message.value).decode('utf-8')))
